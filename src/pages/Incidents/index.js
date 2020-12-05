dfc6a63aa4454f1c732746fae7ca72e32dfa83dd
katarinamai <katarinamassakoinoue@gmail.com>
2020-12-05 15:39:55 -0300

    update

---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
+
+import api from '../../services/api';
+
+export default function Incidents() {
+    
+
+    return(
+        <View style={styles.container} >
+            
+        </View>
+    );
+}
